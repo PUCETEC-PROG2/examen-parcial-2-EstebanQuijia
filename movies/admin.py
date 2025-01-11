4c8d86a2550e426f4c10c39b74c595096c5b9136
Esteban Quijia <eaquijia@puce.edu.ec>
2025-01-11 09:57:03 -0500

    ExamenP2

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.apps import AppConfig
 
 # Register your models here.
+
+class MoviesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'movies'
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=20, null=False) 
+    genre = models.CharField(max_length=10, null=False) 
+    director = models.CharField(max_length=20) 
+    year = models.IntegerField() 
+    synopsis = models.TextField() 
+    
+    def __str__(self):
+        return f"{self.title} - {self.year}"
