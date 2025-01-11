365a4fb9128ac80bea05c1a4a70224a8e24cd340
Esteban Quijia <eaquijia@puce.edu.ec>
2025-01-11 10:50:11 -0500

    ExamenP2

--- movies/models.py
+++ movies/models.py
@@ -3,11 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    title = models.CharField(max_length=20, null=False) 
-    genre = models.CharField(max_length=10, null=False) 
-    director = models.CharField(max_length=20) 
-    year = models.IntegerField() 
+    title = models.CharField(max_length=20) 
+    genre = models.CharField(max_length=15) 
+    director = models.CharField(max_length=12) 
+    release_year = models.IntegerField() 
     synopsis = models.TextField() 
-    
     def __str__(self):
-        return f"{self.title} - {self.year}"
+        return f"{self.title} - {self.release_year}"
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
-# Create your views here.
 
 def movie_list(request):
     movies = Movie.objects.all()
